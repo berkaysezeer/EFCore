bb735983ff9ff0077e0f17eb4ee21ce5f381cce1
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-16 22:35:34 +0300

    Concurrency

---
+++ Concurrency.Web/Controllers/Products.cs
@@ -0,0 +1,69 @@
+﻿using Concurrency.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Concurrency.Web.Controllers
+{
+    public class Products : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public Products(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> Update(int Id)
+        {
+            var product = await _context.Products.FindAsync(Id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product p)
+        {
+            try
+            {
+                _context.Products.Update(p);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                var entry = ex.Entries.First();
+
+                //kullanıcının girdiği değerler
+                var currentProduct = entry.Entity as Product;
+
+                //property listesi döner
+                var databaseValues = entry.GetDatabaseValues();
+
+                //veri tabanındaki değerler
+                var databaseProduct = databaseValues.ToObject() as Product;
+
+                //property listesi döner
+                var client = entry.CurrentValues;
+
+                //eğer farklı bir kullanıcı değeri sildiyse diye kontrol yapıyoruz
+                if (databaseValues == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından silindi");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından güncellendi");
+                    ModelState.AddModelError(string.Empty, $"Güncel değer: {databaseProduct.Name} {databaseProduct.Price} ({databaseProduct.Stock})");
+                }
+
+                return View(p);
+            }
+
+        }
+    }
+}
---
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Concurrency için RowVersion kolonu ayarlıyoruz
+            modelBuilder.Entity<Product>().Property(x => x.RowVersion).IsRowVersion();
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
